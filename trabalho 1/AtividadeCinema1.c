325c99a7fee05efab8c6f06b26783c458098b638
christianst93 <christian.ds.torres@gmail.com>
2026-08-29 09:30:43 -0300

    cmm

--- trabalho 1/AtividadeCinema1.c
+++ trabalho 1/AtividadeCinema1.c
@@ -32,24 +32,14 @@ typedef struct sessao
 // PROTÓTIPOS DAS FUNÇÕES
 
 // Passagem por REFERÊNCIA (Modificam os dados)
-void cadastrarPoltrona(Poltrona *p, int num, char fileira) { 
-    printf("Digite o numero da poltrona (ou 0 para cancelar): ");
-    scanf("%d", &num);
+void cadastrarPoltrona(Poltrona *p, int num, char fileira) {     
 
-    if (num == 0) {
-        printf("Cadastro cancelado pelo usuario.\n");
-        return;
-    }
-
-    if (num < 0 || num > 5)
+    if (num < 1 || num > 5)
     {
         printf("Numero de poltrona invalido para fileira\n");
         return;
     }
-
-    printf("Escolha a fileira: ");
-    scanf(" %c", &fileira);
-
+    
     if (fileira < 'a' || fileira > 'c')
     {
         printf("Fileira invalida!\n");
@@ -58,13 +48,14 @@ void cadastrarPoltrona(Poltrona *p, int num, char fileira) {
 
     p->num = num;
     p->fileira = fileira;
+    p->flag_livre = 1;
 }
 
 void inicializarSala(Sala *s, int numSala) { 
     s->num_sala = numSala;   
     for (int i = 0; i < 15; i++) {
         s->poltronas[i].flag_livre = 1;
-        s->poltronas[i].num = i + 1;
+        s->poltronas[i].num = (i % 5) + 1;
         s->poltronas[i].fileira = 'a' + (i / 5);
     }
 }
@@ -137,17 +128,19 @@ void cancelarCompra(Sessao *s, int posPoltrona){
 void mostrarPoltrona(Poltrona p) {
     printf("Poltrona [%d] | Fileira [%c] | Livre? [%s]\n", p.num, p.fileira, p.flag_livre ? "Sim" : "Nao");
 }
+
 void mostrarMapaSala(Sala s) {
     printf("Sala [%d]\n", s.num_sala);
     for (int i = 0; i < 3; i++)
     {
+        printf("Fileira %c: ", 'a' + i);
         for (int j = 0; j < 5; j++)
         {
-            printf(" [] ");
+            Poltrona pol = s.poltronas[i * 5 + j];
+            printf("%s", pol.flag_livre ? " [ ] " : " [X] ");
         }
-        
+        printf("\n");
     }
-    
 }
 
 void mostrarData(Data d) {
@@ -156,9 +149,11 @@ void mostrarData(Data d) {
 
 void mostrarSessao(Sessao s) {
     printf("\nFilme: %s\n", s.nomeFilme);
-    printf("Data: %d/%d/%d\n", s.dataFilme.dia, s.dataFilme.mes, s.dataFilme.ano);
+    mostrarData(s.dataFilme);
     printf("Hora: %d\n", s.horaFilme);
     printf("Sala: %d\n", s.salaReservada.num_sala);
+
+    mostrarMapaSala(s.salaReservada);
 }
 
 int mostrarMenu()
@@ -184,14 +179,51 @@ int main()
     // 1. Reserva do vetor de 4 sessões no cinema
     Sessao sessoes[4];
 
-   Sessao sessao1, sessao2, sessao3, sessao4;
+    Sessao sessao1, sessao2, sessao3, sessao4;
     Sala sala1, sala2, sala3, sala4;
-    Data ontem, hoje, amanha;    
+    Data ontem, hoje, amanha;
+    
+    Poltrona p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15;
 
+    cadastrarPoltrona(&p1, 1, 'a');
+    cadastrarPoltrona(&p2, 2, 'a');
+    cadastrarPoltrona(&p3, 3, 'a');
+    cadastrarPoltrona(&p4, 4, 'a');
+    cadastrarPoltrona(&p5, 5, 'a');
+
+    cadastrarPoltrona(&p6, 1, 'b');
+    cadastrarPoltrona(&p7, 2, 'b');
+    cadastrarPoltrona(&p8, 3, 'b');
+    cadastrarPoltrona(&p9, 4, 'b');
+    cadastrarPoltrona(&p10, 5, 'b');
+
+    cadastrarPoltrona(&p11, 1, 'c');
+    cadastrarPoltrona(&p12, 2, 'c');
+    cadastrarPoltrona(&p13, 3, 'c');
+    cadastrarPoltrona(&p14, 4, 'c');
+    cadastrarPoltrona(&p15, 5, 'c');
+    
     inicializarSala(&sala1, 1);
     inicializarSala(&sala2, 2);
     inicializarSala(&sala3, 3);
     inicializarSala(&sala4, 4);
+
+    
+    sala1.poltronas[0] = p1;
+    sala1.poltronas[1] = p2;
+    sala1.poltronas[2] = p3;
+    sala1.poltronas[3] = p4;
+    sala1.poltronas[4] = p5;
+    sala1.poltronas[5] = p6;
+    sala1.poltronas[6] = p7;
+    sala1.poltronas[7] = p8;
+    sala1.poltronas[8] = p9;
+    sala1.poltronas[9] = p10;
+    sala1.poltronas[10] = p11;
+    sala1.poltronas[11] = p12;
+    sala1.poltronas[12] = p13;
+    sala1.poltronas[13] = p14;
+    sala1.poltronas[14] = p15;
 
     cadastrarData(&ontem, 27, 8, 2026);
     cadastrarData(&hoje, 28, 8, 2026);
@@ -218,31 +250,49 @@ int main()
                 for (int i = 0; i < 4; i++)
                 {
                     mostrarSessao(sessoes[i]);
-                }
-                
+                }                
                 break;
             case 2:
-            {
-                int numeroSessao, numeroPoltrona, posPoltrona;
-                char fileira;
+                {
+                    int numeroSessao, numeroPoltrona, posPoltrona;
+                    char fileira;
 
-                printf("Escolha a sessao (1 a 4): ");
-                scanf("%d", &numeroSessao);                
+                    printf("Escolha a sessao (1 a 4): ");
+                    scanf("%d", &numeroSessao);
+                    
+                    if (numeroSessao < 1 || numeroSessao > 4)
+                    {
+                        printf("Sessao invalida!\n");
+                        break;
+                    }
 
-                printf("\nSessao escolhida: %s - Sala %d\n",
-                    sessoes[numeroSessao - 1].nomeFilme,
-                    sessoes[numeroSessao - 1].salaReservada.num_sala);
+                    printf("\nSessao escolhida: %s - Sala %d\n",
+                        sessoes[numeroSessao - 1].nomeFilme,
+                        sessoes[numeroSessao - 1].salaReservada.num_sala);
 
-                printf("Escolha a fileira (a, b ou c): ");
-                scanf(" %c", &fileira);
+                    printf("Escolha a fileira (a, b ou c): ");
+                    scanf(" %c", &fileira);
 
-                printf("Escolha a poltrona (1 a 5): ");
-                scanf("%d", &numeroPoltrona);
+                    if (fileira < 'a' || fileira > 'c')
+                    {
+                        printf("Fileira invalida! Escolha entre a, b ou c.\n");
+                        break;
+                    }
 
-                posPoltrona = (fileira - 'a') * 5 + numeroPoltrona - 1;
-                comprarPoltrona(&sessoes[numeroSessao - 1], posPoltrona);
-                break;
-            }
+                    printf("Escolha a poltrona (1 a 5): ");
+                    scanf("%d", &numeroPoltrona);
+
+                    if (numeroPoltrona < 1 || numeroPoltrona > 5)
+                    {
+                        printf("Poltrona invalida! Escolha entre 1 e 5.\n");
+                        break;
+                    }
+
+                    posPoltrona = (fileira - 'a') * 5 + numeroPoltrona - 1;
+                    mostrarPoltrona(sessoes[numeroSessao - 1].salaReservada.poltronas[posPoltrona]);
+                    comprarPoltrona(&sessoes[numeroSessao - 1], posPoltrona);
+                    break;
+                }
             case 3:
             {
                 int numeroSessao, numeroPoltrona;
